fe5a8a9aa49dac5e7b70fcc38ee80d219ab013db
Abno <abhinavmaurya605@gmail.com>
2024-07-17 21:12:19 +0530

    updated

--- src/controllers/Admin/Auth/register.controller.js
+++ src/controllers/Admin/Auth/register.controller.js
@@ -1,10 +1,11 @@
-import { asyncHandler } from "@utils/asyncHandler";
+import { asyncHandler } from "@utils/asyncHandler.js";
 import { registerAdmin } from "@services/Admin/Auth/register.service.js";
 import { ApiResponse } from "@utils/ApiResponse.js";
 
 const register = asyncHandler(async (req, res) => {
   const adminData = req.body;
-  const admin = await registerAdmin(adminData);
+  console.log(adminData);
+  // const admin = await registerAdmin(adminData);
 
   return res.status(201).json(new ApiResponse(201, admin, "Admin registered"));
 });
